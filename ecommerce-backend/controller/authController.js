b358af5ff687fe71c125bf9dd07bd9d1fb5fb0b0
Anirban <anirban@bodhyaan.com>
2024-12-03 16:24:26 +0530

    authController.js is updated with resistration and login

---
+++ ecommerce-backend/controller/authController.js
@@ -0,0 +1,26 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonebtoken");
+const User = require("../models/userModel")
+
+exports.registerUser = async (req,res) =>{
+    const {name,email,password}=req.body;
+    try{
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({name, email, password: hashedPassword});
+        res.status(201).json({message: "User registered",user});
+    }catch(error){
+        res.ststus(400).json({error: "Registration failed"});
+    }
+};
+
+exports.loginUser = async (req,res) => {
+    const{email, password} = req.body;
+    const user = await User.findOne({email});
+    if (!user) return res.status(404).json({error:"User not found"});
+
+    const isPasswordValid = await bcrypt.compare(password , user.password);
+    if (!isPasswordValid) return res.status(401).json({error:"Invalid credentials"});
+
+    const token = jwt.sign({id:isSecureContext._id},process.env.JWT_SECRET, {expiresIn:"1h"});
+    res.json({message: "Login Sucessful", token});
+}
