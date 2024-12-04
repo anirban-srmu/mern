9cabeceda0d8445ed3fc48819732c04afdc9e3b7
Anirban <anirban@bodhyaan.com>
2024-12-04 13:18:16 +0530

    product model and routes updated

--- ecommerce-backend/models/productModel.js
+++ ecommerce-backend/models/productModel.js
@@ -2,9 +2,13 @@ const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
     name: {type:String, required: true},
-    description: String,
+    description: {type: String,require:true},
     price: {type:Number, required: true},
     stock:{type:Number, default: 0},
+    imageUrl:{type:String},
+    category:{type:String, required: true},
+
 });
 
 module.exports = mongoose.model("Product", productSchema);
+module.exports = Products;
--- ecommerce-backend/routes/productRoutes.js
+++ ecommerce-backend/routes/productRoutes.js
@@ -3,7 +3,10 @@ const express = require("express");
 const{createProduct, getProducts} = require("../controller/productController");
 const router = express.Router();
 
-router.post("/",createProduct);
-router.get("/",getProducts);
+router.post("/create",createProduct);
+router.get("/",getAllProducts);
+router.get("/:id",getProductById);
+router.put("/:id", updateProduct);
+router.delete("/:id",deleteProduct);
 
 module.exports = router;
