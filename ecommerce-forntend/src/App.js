9bb8f1f6d730f257456e7ec4641ba8dcc1e0a827
Anirban <anirban@bodhyaan.com>
2024-12-05 14:49:25 +0530

    app.js in frontend is updated

--- ecommerce-forntend/src/App.js
+++ ecommerce-forntend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import{BrowserRouter as Router,Route, Routes} from "react-route-dom";
+import Header from "./components/Header";
+import ProductList from "./components/ProductList";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import ProductDetails from "./components/ProductDetails";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+  return(
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element ={<ProductList />} />
+        <Route path="/login" element ={<Login />} />
+        <Route path="/register" element ={<Register />} />
+        <Route path="product/:id/" element ={<ProductDetails />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
