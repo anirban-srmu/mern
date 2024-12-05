cd1389cc5454c0c4f13db7cce056f2337f9e1a8b
Anirban <anirban@bodhyaan.com>
2024-12-05 10:46:50 +0530

    authMiddleware updated

--- ecommerce-backend/middleware/authMiddleware.js
+++ ecommerce-backend/middleware/authMiddleware.js
@@ -1,15 +1,15 @@
 const jwt = require("jsonwebtoken");
 
-const protect = (req, res, next) => {
+const authMiddleware = (req, res, next) => {
     const token = req.header("Authorrization")?.spli(" ")[1];
     if (!token) return res.status(401).json({error:"Not authorized!"});
 
     try{
-        const decode = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
     }catch {
         res.status(401).json(error:"Invalid Token");
     }
 };
-module.exports = protect;
+module.exports = authMiddleware;
