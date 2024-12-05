192b7fe6dff1b5065446228ac3bb1acbcc7e0f91
Anirban <anirban@bodhyaan.com>
2024-12-05 10:45:21 +0530

    authcontroller updated

--- ecommerce-backend/controller/authController.js
+++ ecommerce-backend/controller/authController.js
@@ -1,26 +1,38 @@
-const bcrypt = require("bcrypt");
+
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel")
 
-exports.registerUser = async (req,res) =>{
-    const {name,email,password}=req.body;
-    try{
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await User.create({name, email, password: hashedPassword});
-        res.status(201).json({message: "User registered",user});
+const registerUser = async (req,res) =>{
+    try {
+        const{name,email,password} = req.body;
+        const userExits = await User.findOne({email});
+        if(userExists){
+            return res.status(400).json({message:"User already exists"});
+        }
+        const newUser = new User({name,email,password});
+        await newUser.save();
+        res.status(201).json({message : "User registered sucessfully"});
     }catch(error){
-        res.ststus(400).json({error: "Registration failed"});
+        res.status(500).json({message: error.message});
+    }
+}
+
+const loginUser = async (req,res) =>{
+    try{
+        const {email,password}= req.body;
+        const user = await User.findOne({email});
+        if (!user){
+            return res.status(400).json({message: 'Invalid credentials'});
+        }
+        const isMatch = await user.comparePassword(password);
+        if(!isMatch){
+            return res.status(400).json({message: 'Invalid credentials'});
+        }
+        const token = jwt.sign({id: user._id,role:user.role},process.env.JWT_SECRET,{expiresIn:'1h'});
+        res.status(200).json({token});
+    }catch(error){
+        res.status(500).json({message: error.message});
     }
 };
 
-exports.loginUser = async (req,res) => {
-    const{email, password} = req.body;
-    const user = await User.findOne({email});
-    if (!user) return res.status(404).json({error:"User not found"});
-
-    const isPasswordValid = await bcrypt.compare(password , user.password);
-    if (!isPasswordValid) return res.status(401).json({error:"Invalid credentials"});
-
-    const token = jwt.sign({id:isSecureContext._id},process.env.JWT_SECRET, {expiresIn:"1h"});
-    res.json({message: "Login Sucessful", token});
-}
+module.exports = {registerUser,loginUser}
